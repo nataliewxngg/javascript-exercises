591f13bf65d2e35abd3a42c594f7bfa83bda064b
nataliewxngg <wong.lt.natalie@gmail.com>
2024-12-29 23:39:54 -0500

    JS Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let out = "";
+    for (let i = string.length - 1; i >= 0; --i) {
+        out += string.charAt(i);
+    }
+    return out;
 };
 
 // Do not edit below this line
