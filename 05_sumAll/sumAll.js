78eab038aeec3de37a3393a796bd02592a6fc684
nataliewxngg <wong.lt.natalie@gmail.com>
2024-12-30 00:01:39 -0500

    JS Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(m, n) {
+    let out = 0;
+    
+    if ((typeof m != "number" || typeof n != "number") || (m < 0 || n < 0) ||
+        (!Number.isInteger(m) || !Number.isInteger(n))) {
+        return "ERROR";
+    }
+    else if (m > n) {
+        const tmp = m;
+        m = n;
+        n = tmp;
+    }
+    for (let i = m; i <= n; ++i) out += i;
+    return out;
 };
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
