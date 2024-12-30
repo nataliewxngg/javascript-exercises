e4bfa5af3622e245c75336adf1497e7bb1097211
nataliewxngg <wong.lt.natalie@gmail.com>
2024-12-29 22:57:28 -0500

    JS Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    if (num < 0) return "ERROR";
 
+    let out = "";
+    for (num; num > 0; --num) 
+        out += string;
+    return out;
 };
+
+console.log(repeatString("hi", 3));
 
 // Do not edit below this line
 module.exports = repeatString;
