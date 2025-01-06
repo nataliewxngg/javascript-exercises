524c42f4a7c92e819b31cba546ade080aefb02ce
nataliewxngg <wong.lt.natalie@gmail.com>
2025-01-06 17:31:12 -0500

    JS Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let oldestPerson;
+    
+    arr.reduce((total, curr) => {
+        let currAge;
+        if (curr.yearOfDeath == undefined) currAge = (new Date()).getFullYear() - curr.yearOfBirth;
+        else currAge = curr.yearOfDeath - curr.yearOfBirth;
 
+        if (currAge > total) {
+            oldestPerson = curr;
+            return currAge;
+        }
+        return total;
+    }, 0);
+    
+    return oldestPerson;
 };
 
 // Do not edit below this line
