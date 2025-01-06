84fc839b9d2477f0d979bcb175f333e2d99b2ae9
nataliewxngg <wong.lt.natalie@gmail.com>
2025-01-06 13:53:55 -0500

    JS Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase().replace(/\s/g, "").replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+    return str.toLowerCase().split("").reverse().join("") === str;
 };
+
+console.log(palindromes("A car, a man, a maraca."));
 
 // Do not edit below this line
 module.exports = palindromes;
