bd33b9237d2dd1f5984d15db39058c1d2cf7663f
nataliewxngg <wong.lt.natalie@gmail.com>
2025-01-06 14:06:20 -0500

    JS Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (typeof num == "string") num = Number(num);
+    if (num >= 0 && num < 2) return num;
+    else if (num == 2) return 1;
+    else if (num < 0) return "OOPS";
+    let prev = 1;
+    let pprev = 1;
+    
+    for (let i = 3; i < num; ++i) {
+        let tmp = prev;
+        prev = prev + pprev;
+        pprev = tmp;
+    }
+    return prev + pprev;
 };
+
+console.log(fibonacci("2"));
 
 // Do not edit below this line
 module.exports = fibonacci;
